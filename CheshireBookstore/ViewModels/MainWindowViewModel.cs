f7c6867fcf2d04f5bf80cc00089c3b4c86a59026
elizabeth-tech <elizabeth-tech@gmail.com>
2021-03-18 19:51:04 +0700

    Созданы оновные папки. Создана модель-представление главного окна и локатор моделей-представления

---
+++ CheshireBookstore/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MathCore.ViewModels;
+
+namespace CheshireBookstore.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Свойства
+
+        #region Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
+        private string title = "Книжный магазин \"Чешир\"";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
