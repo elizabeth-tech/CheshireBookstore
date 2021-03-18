c9f87a3747df89bbe8566fda9cefec84612fdc3a
elizabeth-tech <elizabeth-tech@gmail.com>
2021-03-18 22:21:17 +0700

    Создание контекста БД

--- Bookstore.Lib/Context/BookstoreDbContext.cs
+++ Bookstore.Lib/Context/BookstoreDbContext.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Bookstore.Lib.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookstore.Lib.Context
 {
-    class BookstoreDbContext
+    class BookstoreDbContext : DbContext
     {
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options) : base(options) { }
+
+        // Свойства, которые хранят наборы объектов DbSet<T>. Через них осуществляется связь  с таблицами в бд
+
+        public DbSet<Deal> Deals { get; set; } // можно было бы указать одно свойство, т.к EF сам поймет, что нужно создать связанные с Deal сущности
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Buyer> Buyers { get; set; }
+
+        public DbSet<Seller> Sellers { get; set; }
     }
 }
