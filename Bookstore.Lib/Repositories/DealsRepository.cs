6ffd3e1852439261d5b0ee1cbba418e20e1997cc
elizabeth-tech <elizabeth-tech@gmail.com>
2021-03-19 12:41:03 +0700

    Доделана реализация репозитория сделок

--- Bookstore.Lib/Repositories/DealsRepository.cs
+++ Bookstore.Lib/Repositories/DealsRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace Bookstore.Lib.Repositories
+﻿using Bookstore.Lib.Context;
+using Bookstore.Lib.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Bookstore.Lib.Repositories
 {
-    internal class DealsRepository
+    class DealsRepository : DbRepository<Deal>
     {
+        public DealsRepository(BookstoreDbContext db) : base(db) { }
+        
+        // Переопределяем свойство items. Т.е. при загрузке сделок, мы еще хотим включать информацию о книгах, продавцах и покупателях
+        public override IQueryable<Deal> items => base.items
+            .Include(item => item.Book)
+            .Include(item => item.Seller)
+            .Include(item => item.Buyer)
+            ;
     }
 }
